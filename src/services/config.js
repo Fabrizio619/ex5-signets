a7cf2874aa445991bb60f6a43f3c7c4d0dba3b71
Fabrizio Herrera <javierherrera619@outlook.com>
2021-03-25 10:45:51 -0400

    config et gh-pages

--- src/services/config.js
+++ src/services/config.js
@@ -4,8 +4,16 @@
 */
 
 // Configuration Firebase (mettez-y les vôtres !)
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  
+  apiKey: "AIzaSyDmvduajDhL68xQPkVCbPfCF7lVwAzhjLM",
+  authDomain: "pvt-h21-fabrizio.firebaseapp.com",
+  databaseURL: "https://pvt-h21-fabrizio-default-rtdb.firebaseio.com",
+  projectId: "pvt-h21-fabrizio",
+  storageBucket: "pvt-h21-fabrizio.appspot.com",
+  messagingSenderId: "818019251943",
+  appId: "1:818019251943:web:953668fd7a7af1b622d442",
+  measurementId: "G-CMHQHR79XP"
 };
 export default firebaseConfig;
 
